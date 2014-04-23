7b055edbe4657a09cd2801b856be5bf59ce3de6b
BenStoever <stoever@bioinfweb.info>
2014-04-23 10:03:13 +0000

    Adjustments to package name changes during the creation of bioinfweb.commons in SWT demo project.

--- demo/swt/src/info/bioinfweb/libralign/demo/swt/Main.java
+++ demo/swt/src/info/bioinfweb/libralign/demo/swt/Main.java
@@ -19,8 +19,14 @@
 package info.bioinfweb.libralign.demo.swt;
 
 
+import info.bioinfweb.commons.bio.biojava3.alignment.SimpleAlignment;
+import info.bioinfweb.commons.bio.biojava3.alignment.template.Alignment;
 import info.bioinfweb.libralign.AlignmentArea;
+import info.bioinfweb.libralign.AlignmentSourceDataType;
+import info.bioinfweb.libralign.sequenceprovider.BioJavaSequenceDataProvider;
 
+import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.compound.NucleotideCompound;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -71,6 +77,23 @@ public class Main {
 	}
 	
 	
+	private AlignmentArea createAlignmentArea() {
+		Alignment<DNASequence, NucleotideCompound> alignment = 
+				new SimpleAlignment<DNASequence, NucleotideCompound>();
+		alignment.add("Sequence 1", new DNASequence("ATCGTAGATCGTAGATCGTAGATCGTAGATCGTAGATCGTAGATCGTAGATCGTAGATCGTAG"));
+		alignment.add("Sequence 2", new DNASequence("AT-GTTG"));
+		alignment.add("Sequence 3", new DNASequence("AT-GTAG"));
+		
+		BioJavaSequenceDataProvider<DNASequence, NucleotideCompound> sequenceProvider = 
+				new BioJavaSequenceDataProvider<DNASequence, NucleotideCompound>(
+						alignment, AlignmentSourceDataType.NUCLEOTIDE);
+		
+		AlignmentArea result = new AlignmentArea();
+		result.setSequenceProvider(sequenceProvider, false);
+		return result;
+	}
+	
+	
 	/**
 	 * Create contents of the window.
 	 */
@@ -84,7 +107,7 @@ public class Main {
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
 		
-		Composite alignmentWidget = new AlignmentArea().createSWTWidget(scrolledComposite, SWT.NONE);
+		Composite alignmentWidget = createAlignmentArea().createSWTWidget(scrolledComposite, SWT.NONE);
 		scrolledComposite.setContent(alignmentWidget);
 		scrolledComposite.setMinSize(alignmentWidget.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 	}
