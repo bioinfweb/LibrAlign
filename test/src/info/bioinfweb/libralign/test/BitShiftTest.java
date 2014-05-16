3ab63da71652fba0f2ef7cec64a7d0c6b3851863
BenStoever <stoever@bioinfweb.info>
2014-05-16 14:44:54 +0000

    Test classes for design decisions in implementing BitLists added to test project.

---
+++ test/src/info/bioinfweb/libralign/test/BitShiftTest.java
@@ -0,0 +1,53 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.libralign.test;
+
+
+
+public class BitShiftTest {
+  public static void main(String[] args) {
+//		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(Integer.MIN_VALUE);
+//		System.out.println(Integer.MIN_VALUE >> 1);
+//		System.out.println(Integer.MIN_VALUE >>> 1);
+//		System.out.println(Integer.MIN_VALUE << 1);
+//		int full = 0b11111111111111111111111111111111;  // -1
+//		System.out.println(full);
+//		full = 0b01111111111111111111111111111111;  // MAX_VALUE
+//		System.out.println(full);
+//		full = 0b10000000000000000000000000000000;  // MIN_VALUE
+//		System.out.println(full);
+//		System.out.println();
+	
+  	// Reading
+		int value = 0b00000001000000100000001100000100;
+		int mask = 0b00000000000000000000000011111111; 
+		System.out.println(value + " " + mask);
+		System.out.println((value >> 24) & mask);
+		System.out.println((value >> 16) & mask);
+		System.out.println((value >> 8) & mask);
+		System.out.println(value & mask);
+		
+		// Writing:
+		//V  0010
+		//N  0001
+		//  ~1110
+		//  
+	}	
+}
---
+++ test/src/info/bioinfweb/libralign/test/PrimitiveIntegersPerformanceTest.java
@@ -0,0 +1,103 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.libralign.test;
+
+
+
+public class PrimitiveIntegersPerformanceTest {
+	public static final int SIZE = 1024 * 1024 * 32;
+	
+	
+	private static void testWithInt() {
+  	byte[] byteArray = new byte[SIZE];
+  	byte[] shortArray = new byte[SIZE];
+  	byte[] intArray = new byte[SIZE];
+  	byte[] longArray = new byte[SIZE];
+  	
+		long start = System.currentTimeMillis();
+		for (int repeat = 0; repeat < 100; repeat++) {
+			for (int i = 0; i < byteArray.length; i++) {
+				byteArray[i] = 23;
+			}
+		}
+		long time = System.currentTimeMillis() - start;
+		System.out.println("byte: " + time);
+		
+		start = System.currentTimeMillis();
+		for (int repeat = 0; repeat < 100; repeat++) {
+			for (int i = 0; i < shortArray.length; i++) {
+				shortArray[i] = 23;
+			}
+		}
+		time = System.currentTimeMillis() - start;
+		System.out.println("short: " + (time / 2));
+		
+		start = System.currentTimeMillis();
+		for (int repeat = 0; repeat < 100; repeat++) {
+			for (int i = 0; i < intArray.length; i++) {
+				intArray[i] = 23;
+			}
+		}
+		time = System.currentTimeMillis() - start;
+		System.out.println("int: " + (time / 4));
+		
+		start = System.currentTimeMillis();
+		for (int repeat = 0; repeat < 100; repeat++) {
+			for (int i = 0; i < longArray.length; i++) {
+				longArray[i] = 23;
+			}
+		}
+		time = System.currentTimeMillis() - start;
+		System.out.println("long: " + (time / 8));
+	}
+	
+	
+	private static void testWithAccording() {
+  	byte[] intArray = new byte[SIZE];
+  	byte[] longArray = new byte[SIZE];
+  	
+		long start = System.currentTimeMillis();
+		for (int repeat = 0; repeat < 100; repeat++) {
+			int secondIndex = 0;
+			for (int i = 0; i < intArray.length; i++) {
+				intArray[i] = 23;
+				secondIndex++;
+			}
+		}
+		long time = System.currentTimeMillis() - start;
+		System.out.println("int: " + (time / 4));
+		
+		start = System.currentTimeMillis();
+		for (long repeat = 0; repeat < 100; repeat++) {
+			int secondIndex = 0;
+			for (long i = 0; i < longArray.length; i++) {
+				longArray[secondIndex] = 23;
+				secondIndex++;
+			}
+		}
+		time = System.currentTimeMillis() - start;
+		System.out.println("long: " + (time / 8));
+	}
+	
+	
+  public static void main(String[] args) {
+		testWithAccording();
+		// => Storing binary data in double arrays seems to be far more efficient.
+	}	
+}
