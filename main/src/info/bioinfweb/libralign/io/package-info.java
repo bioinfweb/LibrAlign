cc2906f990f2f9c5104cf632b4e5fe043246bc50
BenStoever <stoever@bioinfweb.info>
2014-09-19 14:15:40 +0000

    NeXML dependency and io package added.

---
+++ main/src/info/bioinfweb/libralign/io/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains classes for reading and writing as well as importing and exporting alignments including there raw and 
+ * meta data supported by LibrAlign.
+ * 
+ * @author Ben St&ouml;ver
+ * @since 0.2.0
+ */
+package info.bioinfweb.libralign.io;
