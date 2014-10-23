6afba5c3f52fd3a3f3d29086182c9b5a1e3206de
BenStoever <stoever@bioinfweb.info>
2014-10-23 09:35:32 +0000

    Pherogram sequence added to EditableAlignmentTest.

--- test/src/info/bioinfweb/libralign/test/editAlignment/EditableAlignmentTest.java
+++ test/src/info/bioinfweb/libralign/test/editAlignment/EditableAlignmentTest.java
@@ -21,6 +21,8 @@ package info.bioinfweb.libralign.test.editAlignment;
 
 import info.bioinfweb.libralign.AlignmentArea;
 import info.bioinfweb.libralign.AlignmentContentArea;
+import info.bioinfweb.libralign.dataarea.implementations.pherogram.PherogramArea;
+import info.bioinfweb.libralign.pherogram.BioJavaPherogramProvider;
 import info.bioinfweb.libralign.sequenceprovider.SequenceDataProvider;
 import info.bioinfweb.libralign.sequenceprovider.implementations.PackedSequenceDataProvider;
 import info.bioinfweb.libralign.sequenceprovider.tokenset.BioJavaTokenSet;
@@ -30,10 +32,16 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
+import org.biojava.bio.chromatogram.ChromatogramFactory;
+import org.biojava.bio.chromatogram.UnsupportedChromatogramFormatException;
 import org.biojava3.core.sequence.compound.DNACompoundSet;
 import org.biojava3.core.sequence.compound.NucleotideCompound;
+
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
+import java.io.File;
+import java.io.IOException;
 
 
 
@@ -74,6 +82,7 @@ public class EditableAlignmentTest {
 		TokenSet<NucleotideCompound> tokenSet = new BioJavaTokenSet<NucleotideCompound>(new DNACompoundSet(), true);
 		SequenceDataProvider<NucleotideCompound> provider = new PackedSequenceDataProvider<NucleotideCompound>(tokenSet);
 		
+		// Test sequence:
 		provider.addSequence("A");
 		int id = provider.sequenceIDByName("A");
 		provider.insertTokenAt(id, 0, tokenSet.tokenByKeyChar('A'));
@@ -82,6 +91,7 @@ public class EditableAlignmentTest {
 		provider.insertTokenAt(id, 3, tokenSet.tokenByKeyChar('-'));
 		provider.insertTokenAt(id, 4, tokenSet.tokenByKeyChar('T'));
 		
+		// Another test sequence:
 		provider.addSequence("B");
 		id = provider.sequenceIDByName("B");
 		provider.insertTokenAt(id, 0, tokenSet.tokenByKeyChar('A'));
@@ -89,6 +99,26 @@ public class EditableAlignmentTest {
 		provider.insertTokenAt(id, 2, tokenSet.tokenByKeyChar('G'));
 		provider.insertTokenAt(id, 3, tokenSet.tokenByKeyChar('G'));
 		provider.insertTokenAt(id, 4, tokenSet.tokenByKeyChar('T'));
+		
+		// Test sequence with pherogram:
+		try {
+			provider.addSequence("C");
+			id = provider.sequenceIDByName("C");
+			BioJavaPherogramProvider pherogramProvider = new BioJavaPherogramProvider(ChromatogramFactory.create(
+	      	new File("data/pherograms/Test_qualityScore.scf")));
+
+			// Copy base call sequence into alignment:
+			for (int i = 1; i <= pherogramProvider.getSequenceLength(); i++) {
+				provider.insertTokenAt(id, i - 1, tokenSet.tokenByKeyChar(pherogramProvider.getBaseCall(i).getUpperedBase().charAt(0)));
+			}
+			
+			// Add data area:
+			PherogramArea pherogramArea = new PherogramArea(result.getContentArea(), pherogramProvider);
+			result.getContentArea().getDataAreas().getSequenceAreas(id).add(pherogramArea);
+		}
+		catch (UnsupportedChromatogramFormatException | IOException e) {
+			e.printStackTrace();
+		}
 		
 		contentArea.setSequenceProvider(provider, false);
 		
