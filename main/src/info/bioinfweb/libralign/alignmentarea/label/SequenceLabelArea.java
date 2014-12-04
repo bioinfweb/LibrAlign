04a3d626cca061a0e4ee0a8015dac4a7a526631e
BenStoever <stoever@bioinfweb.info>
2014-12-04 11:48:08 +0000

    Minor changes.

--- main/src/info/bioinfweb/libralign/alignmentarea/label/SequenceLabelArea.java
+++ main/src/info/bioinfweb/libralign/alignmentarea/label/SequenceLabelArea.java
@@ -55,10 +55,10 @@ public class SequenceLabelArea extends AlignmentLabelSubArea {
 	@Override
 	public void paint(TICPaintEvent e) {
 		Graphics2D g  = e.getGraphics();
-  	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
   			RenderingHints.VALUE_ANTIALIAS_ON);
 
-  	// Paint background:
+  		// Paint background:
 		g.setColor(SystemColor.menu);
 		g.fill(e.getRectangle());
 		
@@ -71,8 +71,8 @@ public class SequenceLabelArea extends AlignmentLabelSubArea {
 		if (getLabeledArea().getOwner().getSequenceOrder().indexByID(getLabeledArea().getSeqenceID()) > 0) {
 			g.draw(new Line2D.Float(0, 0, getSize().width, 0));  // Draw line only if there is another label above.
 		}
-  	g.drawString(contentArea.getSequenceProvider().sequenceNameByID(getLabeledArea().getSeqenceID()), 
-  			AlignmentLabelArea.BORDER_WIDTH, fm.getAscent());
+		g.drawString(contentArea.getSequenceProvider().sequenceNameByID(getLabeledArea().getSeqenceID()), 
+				AlignmentLabelArea.BORDER_WIDTH, fm.getAscent());
 	}
 
 
