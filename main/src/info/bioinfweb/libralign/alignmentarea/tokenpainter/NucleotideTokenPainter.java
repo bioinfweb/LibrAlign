1cebc6d74a7c70d70ecbc6c7167f22673559ef60
BenStoever <stoever@bioinfweb.info>
2015-05-28 10:12:18 +0000

    Intermediate commit in implementing TokenPainter related classes.

--- main/src/info/bioinfweb/libralign/alignmentarea/tokenpainter/NucleotideTokenPainter.java
+++ main/src/info/bioinfweb/libralign/alignmentarea/tokenpainter/NucleotideTokenPainter.java
@@ -1,14 +1,14 @@
 /*
  * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
- * Copyright (C) 2014-2015  Ben Stöver
+ * Copyright (C) 2014 - 2015  Ben Stöver
  * <http://bioinfweb.info/LibrAlign>
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
@@ -16,11 +16,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package info.bioinfweb.libralign.alignmentarea.content;
+package info.bioinfweb.libralign.alignmentarea.tokenpainter;
+
+
+import info.bioinfweb.libralign.model.AlignmentModel;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
 
 
 
-public interface TokenPainter {
-	//TODO Offer methods that allow painting with Graphics2D or GC alternatively.
-	//TODO Create paint event that contains a reference to the AlignmentArea
+public class NucleotideTokenPainter extends AbstractTokenPainter<List<Color>> {
+	@Override
+	public void paintToken(AlignmentModel<?> alignmentModel, Object token, Graphics2D g, Rectangle2D area) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ main/src/info/bioinfweb/libralign/alignmentarea/tokenpainter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains implementations of {@link info.bioinfweb.libralign.alignmentarea.tokenpainter.TokenPainter}
+ *  
+ * @author Ben St&ouml;ver
+ * @since 0.4.0
+ */
+package info.bioinfweb.libralign.alignmentarea.tokenpainter;
