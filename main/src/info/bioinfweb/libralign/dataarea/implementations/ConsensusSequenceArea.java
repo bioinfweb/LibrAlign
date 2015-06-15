5dc72710268c3e4cf21a41f17cd3c8a1d2a952bf
BenStoever <stoever@bioinfweb.info>
2015-06-15 16:06:42 +0000

    Bug fix: ConsensusSequenceArea and SequenceIndexArea are now also repainted of a token in another alignment area is changed.

--- main/src/info/bioinfweb/libralign/dataarea/implementations/ConsensusSequenceArea.java
+++ main/src/info/bioinfweb/libralign/dataarea/implementations/ConsensusSequenceArea.java
@@ -2,42 +2,27 @@
  * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
  * Copyright (C) 2014-2015  Ben Stöver
  * <http://bioinfweb.info/LibrAlign>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package info.bioinfweb.libralign.dataarea.implementations;
 
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.SystemColor;
-import java.awt.geom.Rectangle2D;
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-
-import org.biojava3.core.sequence.compound.NucleotideCompound;
-
 import info.bioinfweb.commons.bio.AmbiguityBaseScore;
-import info.bioinfweb.commons.bio.biojava3.core.sequence.compound.AlignmentAmbiguityNucleotideCompoundSet;
 import info.bioinfweb.commons.tic.TICPaintEvent;
 import info.bioinfweb.libralign.alignmentarea.AlignmentArea;
 import info.bioinfweb.libralign.alignmentarea.content.AlignmentContentArea;
-import info.bioinfweb.libralign.alignmentarea.content.SequenceArea;
 import info.bioinfweb.libralign.dataarea.DataArea;
 import info.bioinfweb.libralign.dataarea.DataAreaListType;
 import info.bioinfweb.libralign.model.AlignmentModel;
@@ -46,47 +31,57 @@ import info.bioinfweb.libralign.model.events.SequenceRenamedEvent;
 import info.bioinfweb.libralign.model.events.TokenChangeEvent;
 import info.bioinfweb.libralign.multiplealignments.MultipleAlignmentsContainer;
 
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.biojava3.core.sequence.compound.NucleotideCompound;
+
 
 
 /**
  * A data area that displays the consensus sequence of an alignment.
- * 
+ *
  * @author Ben St&ouml;ver
  * @since 0.0.0
  */
 public class ConsensusSequenceArea extends DataArea {
 	public static final float DEFAULT_HEIGHT_FACTOR = 3f;
-	
-	
+
+
 	private TreeMap<String, AmbiguityBaseScore> mapByBase;
-	private Map<Integer, AmbiguityBaseScore> scores;
-	
-	
+	private final Map<Integer, AmbiguityBaseScore> scores;
+
+
 	/**
 	 * Creates a new instance of this class that uses the sequence data provider of the specified
 	 * alignment area.
-	 * 
+	 *
 	 * @param owner the alignment area that will be containing the returned data area instance
 	 * @throws IllegalArgumentException if {@code owner} does not have a sequence data provider
 	 */
 	public ConsensusSequenceArea(AlignmentContentArea owner) {
 		this(owner, owner.getOwner());
 	}
-	
-	
+
+
 	/**
 	 * Creates a new instance of this class that uses the specified sequence data provider.
 	 * <p>
-	 * Note that this instance will not react to calls of 
+	 * Note that this instance will not react to calls of
 	 * {@link #afterProviderChanged(AlignmentModel, AlignmentModel)} when this constructor
-	 * is used. Such changes would than have to be done manually by the application code using 
+	 * is used. Such changes would than have to be done manually by the application code using
 	 * {@link #setAlignmentModel(AlignmentModel)}.
-	 * 
+	 *
 	 * @param owner the alignment area that will be containing the returned data area instance
-	 * @param labeledAlignmentArea the alignment area containg the alignment model that shall provide the 
-	 *        source data for the consensus sequence (Should only be different from {@code owner.getOwner()} 
-	 *        if the new instance will be placed in a different alignment area than the sequence data in a 
-	 *        scenario with a {@link MultipleAlignmentsContainer}.) 
+	 * @param labeledAlignmentArea the alignment area containg the alignment model that shall provide the
+	 *        source data for the consensus sequence (Should only be different from {@code owner.getOwner()}
+	 *        if the new instance will be placed in a different alignment area than the sequence data in a
+	 *        scenario with a {@link MultipleAlignmentsContainer}.)
 	 * @throws IllegalArgumentException if {@code sequenceDataProvider} is {@code null}
 	 */
 	public ConsensusSequenceArea(AlignmentContentArea owner, AlignmentArea labeledAlignmentArea) {
@@ -96,7 +91,7 @@ public class ConsensusSequenceArea extends DataArea {
 		assignSize();
 	}
 
-	
+
 	private void createMap() {  //TODO Use ConsensusSequenceCreator instead when implementation is adjusted.
 		mapByBase = new TreeMap<String, AmbiguityBaseScore>();
 		mapByBase.put("A", new AmbiguityBaseScore(1, 0, 0, 0));
@@ -116,8 +111,8 @@ public class ConsensusSequenceArea extends DataArea {
 		mapByBase.put("N", new AmbiguityBaseScore(1, 1, 1, 1, 1));
 		mapByBase.put("-", new AmbiguityBaseScore(0, 0, 0, 0));
 	}
-	
-	
+
+
 	public AmbiguityBaseScore getScore(int column) {
 		AmbiguityBaseScore score = scores.get(column);
 		if (score == null) {
@@ -136,8 +131,8 @@ public class ConsensusSequenceArea extends DataArea {
 		}
 		return score;
 	}
-	
-	
+
+
 	@Override
 	public int getHeight() {
 		return (int)Math.round(DEFAULT_HEIGHT_FACTOR * getLabeledAlignmentArea().getPaintSettings().getTokenHeight());
@@ -151,7 +146,7 @@ public class ConsensusSequenceArea extends DataArea {
 		g.setColor(SystemColor.menu);
 		g.fill(event.getRectangle());
 
-		
+
 		// Determine area to be painted:
 		int firstIndex = Math.max(0, getLabeledAlignmentArea().getContentArea().columnByPaintX((int)event.getRectangle().getMinX()));
 		int lastIndex = getLabeledAlignmentArea().getContentArea().columnByPaintX((int)event.getRectangle().getMaxX());
@@ -159,18 +154,18 @@ public class ConsensusSequenceArea extends DataArea {
 		if ((lastIndex == -1) || (lastIndex > lastColumn)) {  //TODO Elongate to the length of the longest sequence and paint empty/special tokens on the right end?
 			lastIndex = lastColumn;
 		}
-		
+
 		//TODO Refactor nucleotide independent:
 		// Paint output:
 //		Map<String, Color> map = getOwner().getOwner().getColorSchema().getNucleotideColorMap();
 //    Color[] bgColors = new Color[]{map.get("A"), map.get("T"), map.get("C"), map.get("G")};
-//    
-//		AlignmentAmbiguityNucleotideCompoundSet compoundSet =  
+//
+//		AlignmentAmbiguityNucleotideCompoundSet compoundSet =
 //				AlignmentAmbiguityNucleotideCompoundSet.getAlignmentAmbiguityNucleotideCompoundSet();
 //		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 //  	float x = firstIndex * getOwner().getOwner().getCompoundWidth() + getOwner().getOwner().getDataAreas().getGlobalMaxLengthBeforeStart();
 //		float sequenceY = getHeight() - getOwner().getOwner().getCompoundHeight();
-//		final float barWidth = getOwner().getOwner().getCompoundWidth() / 4; 
+//		final float barWidth = getOwner().getOwner().getCompoundWidth() / 4;
 //		for (int i = firstIndex; i <= lastIndex; i++) {
 //			// Paint bars:
 //			float barX = x;
@@ -181,48 +176,49 @@ public class ConsensusSequenceArea extends DataArea {
 //				g.fill(new Rectangle2D.Float(barX, sequenceY - barHeight, barWidth, barHeight));
 //				barX += barWidth;
 //			}
-//			
+//
 //			// Paint token:
-//			SequenceArea.paintCompound(getOwner().getOwner(), event.getGraphics(), 
+//			SequenceArea.paintCompound(getOwner().getOwner(), event.getGraphics(),
 //					compoundSet.getCompoundForString("" + score.getConsensusBase()), x, sequenceY, false);
-//			
+//
 //	    x += getOwner().getOwner().getCompoundWidth();
 //    }
 	}
 
-	
+
 	@Override
 	public Set<DataAreaListType> validLocations() {
 		return EnumSet.of(DataAreaListType.TOP, DataAreaListType.BOTTOM);
 	}
-	
-	
+
+
 	private void refreshConsensus() {
 		scores.clear();  // Each score is recalculated the next time it is requested.
 		assignSize();
+		repaint();
 	}
 
-	
+
 	@Override
 	public void afterTokenChange(TokenChangeEvent e) {
 		if (e.getSource().equals(getLabeledAlignmentModel())) {  // The owner always delegates this call, also if its sequence provider is not used.
 			refreshConsensus();
 		}
 	}
-	
-	
+
+
 	@Override
 	public void afterSequenceRenamed(SequenceRenamedEvent e) {}
-	
-	
+
+
 	@Override
 	public void afterSequenceChange(SequenceChangeEvent e) {
 		if (e.getSource().equals(getLabeledAlignmentModel())) {  // The owner always delegates this call, also if its sequence provider is not used.
 			refreshConsensus();
 		}
 	}
-	
-	
+
+
 	@Override
 	public void afterProviderChanged(AlignmentModel previous,	AlignmentModel current) {
 		refreshConsensus();
--- main/src/info/bioinfweb/libralign/dataarea/implementations/SequenceIndexArea.java
+++ main/src/info/bioinfweb/libralign/dataarea/implementations/SequenceIndexArea.java
@@ -2,34 +2,22 @@
  * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
  * Copyright (C) 2014-2015  Ben Stöver
  * <http://bioinfweb.info/LibrAlign>
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package info.bioinfweb.libralign.dataarea.implementations;
 
-
-import java.awt.BasicStroke;
-import java.awt.FontMetrics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.Stroke;
-import java.awt.SystemColor;
-import java.awt.geom.Line2D;
-import java.awt.geom.Path2D;
-import java.util.EnumSet;
-import java.util.Set;
 
 import info.bioinfweb.commons.Math2;
 import info.bioinfweb.commons.collections.ListChangeType;
@@ -44,6 +32,18 @@ import info.bioinfweb.libralign.model.events.SequenceChangeEvent;
 import info.bioinfweb.libralign.model.events.SequenceRenamedEvent;
 import info.bioinfweb.libralign.model.events.TokenChangeEvent;
 
+import java.awt.BasicStroke;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.SystemColor;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
+import java.util.EnumSet;
+import java.util.Set;
+
 
 
 /**
@@ -51,39 +51,39 @@ import info.bioinfweb.libralign.model.events.TokenChangeEvent;
  * <p>
  * You would usually use this component in the head area of an {@link AlignmentArea} but it is possible to
  * insert it at any position.
- * 
+ *
  * @author Ben St&ouml;ver
  * @since 0.2.0
  */
 public class SequenceIndexArea extends DataArea {
 	/** The default value returned by {@link #getFirstIndex()} if it has not been changed. */
 	public static final int DEFAULT_FIRST_INDEX = 1;
-	
+
 	/** The stroke used to paint the dashes */
 	public static final Stroke DASH_STROKE = new BasicStroke(0, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
-	
-  /** The length of a dash */
-  public static final double DASH_LENGTH_FACTOR = 0.3f;
-  
-  /** The length of a dash with a label */
-  public static final double LABELED_DASH_LENGTH_FACTOR = 1f;
-  
+
+	/** The length of a dash */
+	public static final double DASH_LENGTH_FACTOR = 0.3f;
+
+	/** The length of a dash with a label */
+	public static final double LABELED_DASH_LENGTH_FACTOR = 1f;
+
 	/** The distance of the labels to the border of the component */
 	public static final double LABEL_TOP_DISTANCE_FACTOR = 0.5f;
 
 	/** The distance of the labels to their dash  */
 	public static final double LABEL_LEFT_DISTANCE_FACTOR = 0.2f;
 
-	
-  private int firstIndex = DEFAULT_FIRST_INDEX;
 
-	
+    private int firstIndex = DEFAULT_FIRST_INDEX;
+
+
 	/**
 	 * Creates a new instance of this class.
-	 * 
+	 *
 	 * @param owner - the alignment area that is going to contain this data area
 	 * @param labeledArea the alignment area displays the sequence which is labeled by the new instance
-	 *        (If {@code null} is specified here, the parent alignment area of {@code owner} will be assumed.)  
+	 *        (If {@code null} is specified here, the parent alignment area of {@code owner} will be assumed.)
 	 */
 	public SequenceIndexArea(AlignmentContentArea owner, AlignmentArea labeledArea) {
 		super(owner, labeledArea);
@@ -92,7 +92,7 @@ public class SequenceIndexArea extends DataArea {
 
 	/**
 	 * Creates a new instance of this class using the parent alignment area of {@code owner} as the labeled area.
-	 * 
+	 *
 	 * @param owner - the alignment area that is going to contain this data area
 	 */
 	public SequenceIndexArea(AlignmentContentArea owner) {
@@ -104,32 +104,32 @@ public class SequenceIndexArea extends DataArea {
 		//TODO implement
 //		g.setColor(SystemColor.textHighlight);
 //		AlignmentComparisonPanelSelection selection = getAlignmentComparisonPanel().getSelection();
-//		int x = getAlignmentComparisonPanel().paintXBycolumn(selection.getFirstPos()); 
-//		g.fill(new Rectangle2D.Double(x, 0, 
+//		int x = getAlignmentComparisonPanel().paintXBycolumn(selection.getFirstPos());
+//		g.fill(new Rectangle2D.Double(x, 0,
 //				getAlignmentComparisonPanel().paintXBycolumn(selection.getLastPos() + 1) - 1 - x, HEIGHT));
 	}
-  
-  
+
+
 	private int calculateLabelInterval(FontMetrics fontMetrics) {
 		double compoundWidth = getLabeledAlignmentArea().getPaintSettings().minTokenWidth();
-		return (int)Math2.roundUp((fontMetrics.stringWidth("0") * 
-				("" + getLabeledAlignmentArea().getGlobalMaxSequenceLength()).length() + 2 * LABEL_LEFT_DISTANCE_FACTOR * compoundWidth) 
+		return (int)Math2.roundUp((fontMetrics.stringWidth("0") *
+				("" + getLabeledAlignmentArea().getGlobalMaxSequenceLength()).length() + 2 * LABEL_LEFT_DISTANCE_FACTOR * compoundWidth)
 				/ compoundWidth);
 	}
-	
-	
+
+
 	@Override
 	public void paint(TICPaintEvent e) {
 		Graphics2D g = e.getGraphics();
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
     		RenderingHints.VALUE_ANTIALIAS_ON);
-  
+
     // Paint background:
     Rectangle visibleRect = e.getRectangle();
 		g.setColor(SystemColor.menu);
 		g.fill(visibleRect);
 		paintSelection(g);
-		
+
     // Paint base line
 		if (getLabeledAlignmentModel() instanceof ConcatenatedAlignmentModel) {
 			throw new InternalError("Support for concatenated models not yet implemented.");
@@ -137,13 +137,13 @@ public class SequenceIndexArea extends DataArea {
 		double compoundWidth = getLabeledAlignmentArea().getPaintSettings().getTokenWidth(0);  //TODO Implement support for concatenated models.
 		g.setColor(SystemColor.menuText);
     g.draw(new Line2D.Double(visibleRect.x, getHeight() - 1, visibleRect.x + visibleRect.width, getHeight() - 1));  // base line
-    
+
     // Paint text data and dashes:
-    final int maxLengthBeforeStart = getLabeledAlignmentArea().getDataAreas().getGlobalMaxLengthBeforeStart(); 
+    final int maxLengthBeforeStart = getLabeledAlignmentArea().getDataAreas().getGlobalMaxLengthBeforeStart();
     double labelLeftDistance = LABEL_LEFT_DISTANCE_FACTOR * compoundWidth;
     g.setFont(getLabeledAlignmentArea().getPaintSettings().getTokenHeightFont());
     int labelInterval = calculateLabelInterval(g.getFontMetrics());
-    double x = Math.max(compoundWidth / 2f,  
+    double x = Math.max(compoundWidth / 2f,
     		visibleRect.x - visibleRect.x % compoundWidth - labelInterval * compoundWidth - compoundWidth / 2f);  // labelInterval is subtracted because partly visible text should also be painted
     Stroke stroke = g.getStroke();
     try {
@@ -152,11 +152,11 @@ public class SequenceIndexArea extends DataArea {
     		double dashLength = DASH_LENGTH_FACTOR * getHeight();
     		long compoundIndex = Math.round((x - maxLengthBeforeStart) / compoundWidth);
     		if ((compoundIndex - 1) % labelInterval == 0) {  // BioJava indices start with 1
-    			g.drawString("" + (compoundIndex + getFirstIndex() - 1), (int)(x + labelLeftDistance),	
+    			g.drawString("" + (compoundIndex + getFirstIndex() - 1), (int)(x + labelLeftDistance),
     					(int)(LABEL_TOP_DISTANCE_FACTOR * getHeight()));
     			dashLength = LABELED_DASH_LENGTH_FACTOR * getHeight();
     		}
-    		
+
       	// Dash output
     		g.setStroke(DASH_STROKE);
     		Path2D path = new  Path2D.Double();
@@ -175,21 +175,21 @@ public class SequenceIndexArea extends DataArea {
     }
 	}
 
-	
+
 	@Override
 	public Set<DataAreaListType> validLocations() {
 		return EnumSet.of(DataAreaListType.TOP, DataAreaListType.SEQUENCE, DataAreaListType.BOTTOM);
   }
-	
+
 
 	/**
 	 * Returns the index that will be displayed for the first column of the alignment area.
 	 * <p>
-	 * The default value is {@value #DEFAULT_FIRST_INDEX}. Note that the methods of {@link AlignmentArea} and associated classes will always 
-	 * use 0 as the index of the first column no matter which value is set here. It is used for the GUI output 
+	 * The default value is {@value #DEFAULT_FIRST_INDEX}. Note that the methods of {@link AlignmentArea} and associated classes will always
+	 * use 0 as the index of the first column no matter which value is set here. It is used for the GUI output
 	 * of this data area only.
-	 * 
-	 * @return the displayed index of the first column 
+	 *
+	 * @return the displayed index of the first column
 	 */
 	public int getFirstIndex() {
 		return firstIndex;
@@ -199,9 +199,9 @@ public class SequenceIndexArea extends DataArea {
 	/**
 	 * Sets a new index that will be displayed for the first column of the alignment area.
 	 * <p>
-	 * The methods of {@link AlignmentArea} and associated classes will always use 0 as the index of the first 
+	 * The methods of {@link AlignmentArea} and associated classes will always use 0 as the index of the first
 	 * column no matter which value is specified here. It is used for the GUI output of this data area only.
-	 * 
+	 *
 	 * @param firstIndex - the new index display for the first column
 	 */
 	public void setFirstIndex(int firstIndex) {
@@ -218,6 +218,7 @@ public class SequenceIndexArea extends DataArea {
 	@Override
 	public <T> void afterSequenceChange(SequenceChangeEvent<T> e) {
 		assignSize();  // Longest sequence could have been deleted or a longer sequence could have been added.
+        repaint();  // In case the size did not change, but the space before the alignment did.
 	}
 
 
@@ -229,6 +230,7 @@ public class SequenceIndexArea extends DataArea {
 	public <T> void afterTokenChange(TokenChangeEvent<T> e) {
 		if (!e.getType().equals(ListChangeType.REPLACEMENT)) {
 			assignSize();  // Length of the longest sequence could have changed.
+			repaint();  // In case the size did not change, but the space before the alignment did.
 		}
 	}
 
@@ -236,5 +238,6 @@ public class SequenceIndexArea extends DataArea {
 	@Override
 	public <T, U> void afterProviderChanged(AlignmentModel<T> previous,	AlignmentModel<U> current) {
 		assignSize();
+		repaint();  // In case the size did not change, but the space before the alignment did.
 	}
 }
