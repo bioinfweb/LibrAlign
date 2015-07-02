4519aa85f661da63c681488f45a12fe51247fe92
BenStoever <stoever@bioinfweb.info>
2015-07-02 16:04:44 +0000

    Test application for empty alignments added.

---
+++ test/src/info/bioinfweb/libralign/test/empty/AbstractEmptyAlignmentTest.java
@@ -0,0 +1,51 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014-2015  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.libralign.test.empty;
+
+
+import info.bioinfweb.libralign.alignmentarea.AlignmentArea;
+import info.bioinfweb.libralign.alignmentarea.tokenpainter.NucleotideTokenPainter;
+import info.bioinfweb.libralign.dataarea.implementations.SequenceIndexArea;
+import info.bioinfweb.libralign.model.AlignmentModel;
+import info.bioinfweb.libralign.model.implementations.PackedAlignmentModel;
+import info.bioinfweb.libralign.model.tokenset.CharacterTokenSet;
+import info.bioinfweb.libralign.multiplealignments.MultipleAlignmentsContainer;
+
+
+
+public class AbstractEmptyAlignmentTest {
+	protected MultipleAlignmentsContainer createContainer() {
+		MultipleAlignmentsContainer result = new MultipleAlignmentsContainer();
+		AlignmentArea labelArea = new AlignmentArea(result);
+		AlignmentArea sequenceArea = new AlignmentArea(result);
+		
+		labelArea.getDataAreas().getTopAreas().add(new SequenceIndexArea(labelArea.getContentArea(), sequenceArea));
+		result.getAlignmentAreas().add(labelArea);
+		
+		AlignmentModel<Character> model = new PackedAlignmentModel<Character>(CharacterTokenSet.newNucleotideInstance()); 
+		sequenceArea.setAlignmentModel(model, false);
+		sequenceArea.getAlignmentModel().addSequence("A");
+		//model.appendToken(model.sequenceIDByName("A"), 'A');
+		result.getAlignmentAreas().add(sequenceArea);
+		
+		sequenceArea.getPaintSettings().getTokenPainterList().set(0, new NucleotideTokenPainter());
+		
+		return result;
+	}
+}
---
+++ test/src/info/bioinfweb/libralign/test/empty/SwingEmptyAlignmentTest.java
@@ -0,0 +1,68 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014-2015  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.libralign.test.empty;
+
+
+import info.bioinfweb.tic.SwingComponentFactory;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+
+public class SwingEmptyAlignmentTest extends AbstractEmptyAlignmentTest {
+	private JFrame frame;
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SwingEmptyAlignmentTest window = new SwingEmptyAlignmentTest();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	/**
+	 * Create the application.
+	 */
+	public SwingEmptyAlignmentTest() {
+		initialize();
+	}
+	
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(SwingComponentFactory.getInstance().getSwingComponent(createContainer()));
+	}
+}
