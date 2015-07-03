8e7340e96009df05bc9d6fb214be3dcbaabfeecf
BenStoever <stoever@bioinfweb.info>
2015-07-03 12:06:37 +0000

    Mouse listener of content scroll container in SWTAlignmentArea now checks if an alignment model is present to avoid a NPE.

--- main/info.bioinfweb.libralign.swt/src/info/bioinfweb/libralign/alignmentarea/SWTAlignmentArea.java
+++ main/info.bioinfweb.libralign.swt/src/info/bioinfweb/libralign/alignmentarea/SWTAlignmentArea.java
@@ -2,25 +2,22 @@
  * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
  * Copyright (C) 2014-2015  Ben Stöver
  * <http://bioinfweb.info/LibrAlign>
- * 
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package info.bioinfweb.libralign.alignmentarea;
 
-
-import java.awt.Dimension;
-import java.awt.Rectangle;
 
 import info.bioinfweb.commons.swt.ScrolledCompositeSyncListener;
 import info.bioinfweb.libralign.alignmentarea.content.AlignmentContentArea;
@@ -30,6 +27,9 @@ import info.bioinfweb.libralign.alignmentarea.label.SWTAlignmentLabelArea;
 import info.bioinfweb.libralign.multiplealignments.SWTMultipleAlignmentsContainer;
 import info.bioinfweb.tic.SWTComponentFactory;
 import info.bioinfweb.tic.toolkit.AbstractSWTComposite;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
@@ -48,25 +48,25 @@ import org.eclipse.swt.widgets.Composite;
  * SWT component displaying the head, content, or bottom area of an alignment area. This class also implements
  * the necessary scrolling functionality and offers workarounds for missing Swing functionality in SWT.
  * <p>
- * This class is meant for internal use by {@link AlignmentArea} and should not be instantiated directly.   
- * 
+ * This class is meant for internal use by {@link AlignmentArea} and should not be instantiated directly.
+ *
  * @author Ben St&ouml;ver
  * @since 0.2.0
  */
 public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpecificAlignmentArea {
-	private AlignmentArea owner;
-	
+	private final AlignmentArea owner;
+
 	private Composite labelContainer;
 	private ScrolledComposite labelScroller;
 	private SWTScrolledCompositeResizeListener labelResizeListener;
 	private Composite contentContainer;
 	private ScrolledComposite contentScroller;
 	private SWTScrolledCompositeResizeListener contentResizeListener;
-  
-  
+
+
 	/**
 	 * Creates a new instance of this class.
-	 * 
+	 *
 	 * @param owner - the alignment area using this component
 	 * @param parent - the parent SWT component
 	 * @param position - the position of this component in {@code owner}
@@ -78,8 +78,8 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 		this.owner = owner;
 		initGUI(false);
 	}
-	
-	
+
+
 	private GridData createGridData(boolean grabExcessHorizontalSpace) {
 		GridData gridData = new GridData();
 		gridData.horizontalAlignment = GridData.FILL;
@@ -88,8 +88,8 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 		gridData.grabExcessVerticalSpace = true;
 		return gridData;
 	}
-	
-	
+
+
 	private void initGUI(boolean hideHorizontalScrollBar) {
 		// Set layout:
 		GridLayout layout = new GridLayout(2, false);
@@ -109,7 +109,7 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 			}
 		});
 		SWTComponentFactory factory = SWTComponentFactory.getInstance();
-		
+
 		// Label components:
 		labelContainer = new Composite(this, SWT.NONE);
 		labelContainer.setLayoutData(createGridData(false));
@@ -118,10 +118,10 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 		SWTAlignmentLabelArea labelArea =  // Will not create any subelements of labelArea because the content area has not yet been created.
 				(SWTAlignmentLabelArea)factory.getSWTComponent(getIndependentComponent().getLabelArea(), labelScroller, SWT.NONE);
 		labelScroller.setContent(labelArea);
-		labelResizeListener = new SWTScrolledCompositeResizeListener(labelContainer, labelScroller, true, 
+		labelResizeListener = new SWTScrolledCompositeResizeListener(labelContainer, labelScroller, true,
 				hideHorizontalScrollBar);
 		labelContainer.addControlListener(labelResizeListener);  // Must not be called before both fields are initialized.
-		
+
 		// Alignment area part components:
 		contentContainer = new Composite(this, SWT.NONE);
 		contentContainer.setLayoutData(createGridData(true));
@@ -133,27 +133,29 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 		contentScroller.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent event) {  // Sets the focus to an according sequence area, if the user clicks outside of the content area and moves the cursor.
-				AlignmentContentArea contentArea = getIndependentComponent().getContentArea();
-				int row = contentArea.rowByPaintY(event.y); 
-				SequenceArea sequenceArea = contentArea.getToolkitComponent().getSequenceAreaByID(
-						getIndependentComponent().getSequenceOrder().idByIndex(row));
-				if (sequenceArea != null) {
-					getIndependentComponent().getSelection().setNewCursorPosition(contentArea.columnByPaintX(event.x), row);
-					((Composite)sequenceArea.getToolkitComponent()).setFocus();
-				}
+                if (getIndependentComponent().hasAlignmentModel()) {
+    				AlignmentContentArea contentArea = getIndependentComponent().getContentArea();
+    				int row = contentArea.rowByPaintY(event.y);
+    				SequenceArea sequenceArea = contentArea.getToolkitComponent().getSequenceAreaByID(
+    						getIndependentComponent().getSequenceOrder().idByIndex(row));
+    				if (sequenceArea != null) {
+    					getIndependentComponent().getSelection().setNewCursorPosition(contentArea.columnByPaintX(event.x), row);
+    					((Composite)sequenceArea.getToolkitComponent()).setFocus();
+    				}
+                }
 			}
 		});
-		contentResizeListener = new SWTScrolledCompositeResizeListener(contentContainer, contentScroller, false, 
-				hideHorizontalScrollBar); 
+		contentResizeListener = new SWTScrolledCompositeResizeListener(contentContainer, contentScroller, false,
+				hideHorizontalScrollBar);
 		contentContainer.addControlListener(contentResizeListener);  // Must not be called before both fields are initialized.
-		
+
 		// Update label area:
 		labelArea.reinsertSubelements();
-		
+
 		// Synchronize vertical scrolling:
 		ScrolledCompositeSyncListener verticalSyncListener = new ScrolledCompositeSyncListener(
 				new ScrolledComposite[]{labelScroller, contentScroller}, false);
-		verticalSyncListener.registerToAll();		
+		verticalSyncListener.registerToAll();
 
 		// Link label area:
 		contentArea.addControlListener(new ControlAdapter() {
@@ -162,7 +164,7 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 						getIndependentComponent().getLabelArea().assignSize();
 					}
 				});
-		
+
 		// Ensure correct width of label components:
 		ControlListener labelAreaResizeListener = new ControlAdapter() {
 					@Override
@@ -177,7 +179,7 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 				};
 		labelArea.addControlListener(labelAreaResizeListener);
 		labelAreaResizeListener.controlResized(null);  // Apply initial size to container.
-		
+
 		// Set correct size to recently created SWT components:
 		getIndependentComponent().assignSizeToAll();
 	}
@@ -203,8 +205,8 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 	public boolean isHideHorizontalScrollBar() {
 		return contentResizeListener.isHideHorizontalBar();
 	}
-	
-	
+
+
 	@Override
 	public void setHideHorizontalScrollBar(boolean hideHorizontalScrollBar) {
 		labelResizeListener.setHideHorizontalBar(hideHorizontalScrollBar);
@@ -216,15 +218,15 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 	public void scrollAlignmentRectToVisible(Rectangle rectangle) {
 		org.eclipse.swt.graphics.Point origin = getContentScroller().getOrigin();
 		Rectangle visibleRect = getVisibleAlignmentRect();
-		
+
 		int x = origin.x;  // Do not scroll
 		if (rectangle.x < visibleRect.x) {
 			x = rectangle.x;  // Scroll left
 		}
 		else if (rectangle.x + rectangle.width > visibleRect.x + visibleRect.width) {
-			x = rectangle.x + rectangle.width - visibleRect.width;  // Scroll right 
+			x = rectangle.x + rectangle.width - visibleRect.width;  // Scroll right
 		}
-		
+
 		int y = origin.y;  // Do not scroll
 		if (rectangle.y < visibleRect.y) {
 			y = rectangle.y;  // Scroll up
@@ -232,24 +234,24 @@ public class SWTAlignmentArea extends AbstractSWTComposite implements ToolkitSpe
 		if (rectangle.y + rectangle.height > visibleRect.y + visibleRect.height) {
 			y = rectangle.y + rectangle.height - visibleRect.height;  // Scroll down
 		}
-		
+
 		getContentScroller().setOrigin(x, y);
 	}
-  
-  
+
+
 	@Override
 	public Rectangle getVisibleAlignmentRect() {
 		org.eclipse.swt.graphics.Rectangle clientArea = getContentScroller().getClientArea();
 		org.eclipse.swt.graphics.Point origin = getContentScroller().getOrigin();
 		return new Rectangle(origin.x, origin.y, clientArea.width, clientArea.height);
 	}
-	
-	
+
+
 	/**
 	 * Returns the height of the horizontal content scroll bar.
 	 * <p>
 	 * This method is used internally by {@link SWTMultipleAlignmentsContainer#getNeededHeight(int)}.
-	 * 
+	 *
 	 * @return the height in pixels
 	 */
 	public int getHorizontalScrollbarHeight() {
