3ac7d80e15e1d320cbd23c004ca9ae05b74986b3
BenStoever <stoever@bioinfweb.info>
2015-09-16 15:58:35 +0000

    Test project adjusted to recent changes in main.

--- test/src/info/bioinfweb/libralign/model/utils/CharSequenceTokenScannerTest.java
+++ test/src/info/bioinfweb/libralign/model/utils/CharSequenceTokenScannerTest.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package info.bioinfweb.libralign.model;
+package info.bioinfweb.libralign.model.utils;
 
 
 import static org.junit.Assert.*;
