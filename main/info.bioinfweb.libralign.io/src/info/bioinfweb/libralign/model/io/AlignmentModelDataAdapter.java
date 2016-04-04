eed129f7303fc6742a4a4b6c217fb610b0aeb75f
BenStoever <stoever@bioinfweb.info>
2016-04-04 13:15:28 +0000

    IO module: NeXML.jar removed. Indirect JPhyloIO dependencies added. AlignmentModelDataAdapter and TokenSetAdapter added.

---
+++ main/info.bioinfweb.libralign.io/src/info/bioinfweb/libralign/model/io/AlignmentModelDataAdapter.java
@@ -0,0 +1,294 @@
+package info.bioinfweb.libralign.model.io;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.semanticweb.owlapi.io.XMLUtils;
+
+import info.bioinfweb.commons.Math2;
+import info.bioinfweb.jphyloio.JPhyloIOEventWriter;
+import info.bioinfweb.jphyloio.ReadWriteConstants;
+import info.bioinfweb.jphyloio.dataadapters.JPhyloIOEventReceiver;
+import info.bioinfweb.jphyloio.dataadapters.MatrixDataAdapter;
+import info.bioinfweb.jphyloio.dataadapters.ObjectListDataAdapter;
+import info.bioinfweb.jphyloio.dataadapters.implementations.EmptyAnnotatedDataAdapter;
+import info.bioinfweb.jphyloio.dataadapters.implementations.EmptyObjectListDataAdapter;
+import info.bioinfweb.jphyloio.events.LabeledIDEvent;
+import info.bioinfweb.jphyloio.events.LinkedLabeledIDEvent;
+import info.bioinfweb.jphyloio.events.SequenceTokensEvent;
+import info.bioinfweb.jphyloio.events.TokenSetDefinitionEvent;
+import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.libralign.model.AlignmentModel;
+import info.bioinfweb.libralign.model.concatenated.ConcatenatedAlignmentModel;
+
+
+
+/**
+ * An <i>JPhyloIO</i> data adapter that allows writing the contents of an implementation of {@link AlignmentModel}
+ * to an implementation of {@link JPhyloIOEventWriter}.
+ * <p>
+ * Whether the sequence start events links an associated OTU or not can be defined by the constructor parameter
+ * {@code linkOTUs}. If OTUs shall be linked, it will be assumed that such an OTU will have an ID consisting of the
+ * concatenation of {@link ReadWriteConstants#DEFAULT_OTU_ID_PREFIX} and the sequence ID used in the underlying 
+ * {@link AlignmentModel}. If a more complex OTU linking is necessary, the method {@link #getLinkedOTUID(int)} can
+ * be overwritten accordingly. 
+ * <p>
+ * This implementation does not write any metadata associated with sequences or the alignment as a whole. If such data
+ * shall be written the methods {@link #writeMetadata(JPhyloIOEventReceiver)} or 
+ * {@link #writeSequenceMetadata(JPhyloIOEventReceiver, String)} should be overwritten accordingly.
+ * 
+ * @author Ben St&ouml;ver
+ *
+ * @param <T> the type of sequence elements (tokens) used by the underlying alignment model object
+ */
+public class AlignmentModelDataAdapter<T> extends EmptyAnnotatedDataAdapter implements MatrixDataAdapter {
+	private static final int MAX_TOKENS_PER_EVENT = 64;
+	
+	
+	private String idPrefix;
+	private LinkedLabeledIDEvent startEvent;
+	private AlignmentModel<T> model;
+	private boolean linkOTUs;
+	private TokenSetAdapter<T> tokenSetAdapter;
+	
+
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param idPrefix the ID prefix to be used for all generated <i>JPhyloIO</i> IDs of sequences and token sets.
+	 *        (Must be a valid <a href="https://www.w3.org/TR/1999/REC-xml-names-19990114/#NT-NCName">NCName</a>.)
+	 * @param startEvent the <i>JPhyloIO</i> alignment start event to be used
+	 * @param model the <i>LibrAlign</i> alignment model which shall be the source for this adapter
+	 * @param linkOTUs Specify {@code true} here, if each sequence shall link an OTU or {@code false} otherwise.
+	 *        (See {@link #getLinkedOTUID(int)} for details on how OTU IDs are generated.) 
+	 */
+	public AlignmentModelDataAdapter(String idPrefix, LinkedLabeledIDEvent startEvent, AlignmentModel<T> model, boolean linkOTUs) {
+		super();
+		if (!XMLUtils.isNCName(idPrefix)) {
+			throw new IllegalArgumentException("The ID prefix  (\"" + idPrefix + "\") is not a valid NCName.");
+		}
+		else if (startEvent == null) {
+			throw new NullPointerException("startEvent must not be null.");
+		}
+		else if (model == null) {
+			throw new NullPointerException("model must not be null.");
+		}
+		else {
+			this.idPrefix = idPrefix;
+			this.startEvent = startEvent;
+			this.model = model;
+			this.linkOTUs = linkOTUs;
+			tokenSetAdapter = new TokenSetAdapter<T>(idPrefix, model.getTokenSet());
+		}
+	}
+
+
+	public String getIDPrefix() {
+		return idPrefix;
+	}
+
+
+	public boolean isLinkOTUs() {
+		return linkOTUs;
+	}
+
+
+	@Override
+	public LinkedLabeledIDEvent getStartEvent() {
+		return startEvent;
+	}
+
+
+	@Override
+	public long getSequenceCount() {
+		return model.getSequenceCount();
+	}
+
+
+	@Override
+	public long getColumnCount() {
+		// TODO Determine whether sequences have unequal lengths
+		return 0;
+	}
+
+
+	@Override
+	public boolean containsLongTokens() {
+		return model.getTokenSet().maxRepresentationLength() > 1;
+	}
+
+
+	/**
+	 * This default implementation returns an empty list.
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public ObjectListDataAdapter<LabeledIDEvent> getCharacterSets() {
+		return EmptyObjectListDataAdapter.SHARED_EMPTY_OBJECT_LIST_ADAPTER;
+	}
+
+
+	@Override
+	public ObjectListDataAdapter<TokenSetDefinitionEvent> getTokenSets() {
+		if (model instanceof ConcatenatedAlignmentModel) {
+			throw new InternalError("Support for concatenated alignment models not implemented.");  //TODO Implement when ConcatenatedAlignmentModel is available
+		}
+		else {
+			return tokenSetAdapter;
+		}
+	}
+	
+	
+	/**
+	 * Returns the <i>JPhyloIO</i> sequence ID to be used for the specified sequence ID used in the underlying model.
+	 * <p>
+	 * This default implementation returns the concatenation of {@link #getIDPrefix()} and {@code sequenceID}. It can be 
+	 * overwritten to use a different pattern.
+	 * 
+	 * @param modelSequenceID the ID of the sequence used in the underlying {@link AlignmentModel} instance
+	 * @return the <i>JPhyloIO</i> sequence ID
+	 */
+	protected String jPhyloIOByModelSequenceID(int modelSequenceID) {
+		return idPrefix + modelSequenceID;
+	}
+
+
+	/**
+	 * Returns the <i>LibrAlign</i> model sequence ID associated with the specified <i>JPhyloIO</i> sequence ID.
+	 * <p>
+	 * This default implementation extracts the integer ID that was used to create such an ID using 
+	 * {@link #jPhyloIOByModelSequenceID(int)}. If {@link #jPhyloIOByModelSequenceID(int)} was overwritten this method
+	 * must be overwritten accordingly.
+	 * <p>
+	 * Note that this method does not test, if the extracted ID actually exists in the underlying <i>LibrAlign</i>
+	 * alignment model.
+	 * 
+	 * @param jPhyloIOsequenceID the sequence ID used by <i>JPhyloIO</i>
+	 * @return the sequence ID used by the underlying alignment model
+	 * @throws IllegalArgumentException if no integer ID can be extracted from the specified ID
+	 */
+	protected int modelByJPhyloIOSequenceID(String jPhyloIOsequenceID) throws IllegalArgumentException {
+		try {
+			return Integer.parseInt(jPhyloIOsequenceID.substring(idPrefix.length()));
+		}
+		catch (NumberFormatException e) {
+			throw new IllegalArgumentException("The specified JPhyloIO sequence ID \"" + jPhyloIOsequenceID + 
+					"\" is invalid an cannot be converted to a model ID.", e);
+		}
+	}
+
+
+	/**
+	 * Returns the ID of the <i>JPhyloIO</i> OTU that shall be linked with the specified sequence.
+	 * <p>
+	 * This default implementation returns the concatenation of {@link ReadWriteConstants#DEFAULT_OTU_ID_PREFIX} and 
+	 * {@code sequenceID} if {@link #isLinkOTUs()} returns {@code true} or {@code null} if {@link #isLinkOTUs()} returns
+	 * {@code false}.
+	 * <p>
+	 * Note that the ID prefix for sequences and token sets, which can be specified in the constructor will not be used 
+	 * for IDs creates by this method.
+	 * <p>
+	 * This method can be overwritten to use a different pattern of OTU linking.
+	 * 
+	 * @param sequenceID the ID of the sequence used in the underlying {@link AlignmentModel} instance
+	 * @return the linked OTU or {@code null} if no OTU shall be linked.
+	 */
+	protected String getLinkedOTUID(int sequenceID) {
+		if (isLinkOTUs()) {
+			return ReadWriteConstants.DEFAULT_OTU_ID_PREFIX + sequenceID;
+		}
+		else {
+			return null;
+		}
+	}
+
+
+	@Override
+	public Iterator<String> getSequenceIDIterator() {
+		final Iterator<Integer> iterator = model.sequenceIDIterator();
+		return new Iterator<String>() {
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			
+			@Override
+			public String next() {
+				return jPhyloIOByModelSequenceID(iterator.next());
+			}
+
+
+			@Override
+			public void remove() {
+				iterator.remove();
+			}
+		};
+	}
+	
+	
+	@Override
+	public LinkedLabeledIDEvent getSequenceStartEvent(String sequenceID) {
+		int modelID = modelByJPhyloIOSequenceID(sequenceID);
+		if (model.containsSequence(modelID)) {
+			return new LinkedLabeledIDEvent(EventContentType.SEQUENCE, sequenceID, model.sequenceNameByID(modelID), 
+					getLinkedOTUID(modelID));
+		}
+		else {
+			throw new IllegalArgumentException("No according sequence to the ID \"" + sequenceID + 
+					"\" could be found in the underlying model.");
+		}
+	}
+
+
+	@Override
+	public long getSequenceLength(String sequenceID) throws IllegalArgumentException {
+		return model.getSequenceLength(modelByJPhyloIOSequenceID(sequenceID));
+	}
+	
+	
+	/**
+	 * Can be used to write metadata associated with a sequence.
+	 * <p>
+	 * This default implementation is empty, but can be overwritten by inherited classes.
+	 * 
+	 * @param receiver the receiver to write the events to
+	 * @param sequenceID the ID of the sequence carrying the metadata
+	 * @throws IOException if an I/O error occurs while {@code receiver} is trying to write the data
+	 * @throws IllegalArgumentException if not sequence with the specified ID exists
+	 */
+	protected void writeSequenceMetadata(JPhyloIOEventReceiver receiver, String sequenceID) 
+			throws IOException,	IllegalArgumentException {}  //TODO Automatically write content of linked data models here (if there are such in future versions of AlignmentModel)?
+
+
+	@Override
+	public void writeSequencePartContentData(JPhyloIOEventReceiver receiver, String sequenceID, long startColumn, long endColumn) 
+			throws IOException,	IllegalArgumentException {
+		
+		int modelID = modelByJPhyloIOSequenceID(sequenceID);
+		int sequenceLength = model.getSequenceLength(modelID); 
+		if (Math2.isBetween(startColumn, 0, sequenceLength - 1) && Math2.isBetween(endColumn, startColumn, sequenceLength)) {
+			if (startColumn == 0) {
+				writeSequenceMetadata(receiver, sequenceID);
+			}
+			
+			ArrayList<String> tokens = new ArrayList<String>(MAX_TOKENS_PER_EVENT);
+			for (int column = (int)startColumn; column < (int)endColumn; column++) {
+				if (tokens.size() >= MAX_TOKENS_PER_EVENT) {
+					receiver.add(new SequenceTokensEvent(tokens));
+					tokens = new ArrayList<String>(MAX_TOKENS_PER_EVENT);
+				}
+				tokens.add(model.getTokenSet().representationByToken(model.getTokenAt(modelID, column)));
+			}
+			if (!tokens.isEmpty()) {
+				receiver.add(new SequenceTokensEvent(tokens));
+			}
+		}
+		else {
+			throw new IndexOutOfBoundsException("The specified start (" + startColumn + ") or end column (" + endColumn + 
+					") was invalid.");  //TODO Allow endColumn to be behind the end of the sequence?  
+		}
+	}
+}
---
+++ main/info.bioinfweb.libralign.io/src/info/bioinfweb/libralign/model/io/TokenSetAdapter.java
@@ -0,0 +1,121 @@
+package info.bioinfweb.libralign.model.io;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.semanticweb.owlapi.io.XMLUtils;
+
+import info.bioinfweb.commons.IntegerIDManager;
+import info.bioinfweb.jphyloio.ReadWriteConstants;
+import info.bioinfweb.jphyloio.dataadapters.JPhyloIOEventReceiver;
+import info.bioinfweb.jphyloio.dataadapters.ObjectListDataAdapter;
+import info.bioinfweb.jphyloio.dataadapters.implementations.EmptyAnnotatedDataAdapter;
+import info.bioinfweb.jphyloio.events.ConcreteJPhyloIOEvent;
+import info.bioinfweb.jphyloio.events.SingleTokenDefinitionEvent;
+import info.bioinfweb.jphyloio.events.TokenSetDefinitionEvent;
+import info.bioinfweb.jphyloio.events.type.EventContentType;
+import info.bioinfweb.libralign.model.tokenset.TokenSet;
+
+
+
+public class TokenSetAdapter<T> extends EmptyAnnotatedDataAdapter implements ObjectListDataAdapter<TokenSetDefinitionEvent> {
+	private String idPrefix; 
+	private TokenSet<T> tokenSet;
+	private String tokenSetID;
+	
+	
+	public TokenSetAdapter(String idPrefix, TokenSet<T> tokenSet) {
+		super();
+		if (!XMLUtils.isNCName(idPrefix)) {
+			throw new IllegalArgumentException("The ID prefix  (\"" + idPrefix + "\") is not a valid NCName.");
+		}
+		else if (tokenSet == null) {
+			throw new NullPointerException("tokenSet must not be null.");
+		}
+		else {
+			this.idPrefix = idPrefix;
+			this.tokenSet = tokenSet;
+			tokenSetID = idPrefix + ReadWriteConstants.DEFAULT_TOKEN_SET_ID_PREFIX;
+		}
+	}
+
+
+	public String getIDPrefix() {
+		return idPrefix;
+	}
+
+
+	public TokenSet<T> getTokenSet() {
+		return tokenSet;
+	}
+
+	
+	private boolean checkTokenSetID(String id) {
+		if (tokenSetID.equals(id)) {
+			return true;
+		}
+		else {
+			throw new IllegalArgumentException("A token set with the ID \"" + id + "\" is not present.");
+		}
+	}
+	
+
+	@Override
+	public TokenSetDefinitionEvent getObjectStartEvent(String id)	throws IllegalArgumentException {
+		checkTokenSetID(id);  // May throw an exception.
+		return new TokenSetDefinitionEvent(tokenSet.getType(), id, null);
+	}
+	
+
+	@Override
+	public long getCount() {
+		return 1;
+	}
+
+	
+	@Override
+	public Iterator<String> getIDIterator() {
+		return Arrays.asList(new String[]{tokenSetID}).iterator();
+	}
+
+	
+	private void writeSingleTokenDefinitionEvent(JPhyloIOEventReceiver receiver, IntegerIDManager idManager, T token) throws IOException {
+		receiver.add(new SingleTokenDefinitionEvent(idPrefix + idManager.createNewID(), tokenSet.descriptionByToken(token), 
+					tokenSet.representationByToken(token), tokenSet.getMeaning(token), tokenSet.getSymbolType(token)));
+		//TODO Possibly add metadata (e.g. for alternative gap symbols) if that will not anyway be done in JPhyloIO in the future.
+		receiver.add(ConcreteJPhyloIOEvent.createEndEvent(EventContentType.SINGLE_TOKEN_DEFINITION));
+	}
+	
+	
+	@Override
+	public void writeContentData(JPhyloIOEventReceiver receiver, String id) throws IOException, IllegalArgumentException {
+		checkTokenSetID(id);  // May throw an exception.
+		IntegerIDManager idManager = new IntegerIDManager();
+		
+		T defaultGapToken = tokenSet.getGapToken();
+		if (defaultGapToken != null) {
+			writeSingleTokenDefinitionEvent(receiver, idManager, defaultGapToken);  // Make sure the default gap token in the first in the sequence.
+		}
+		
+		T defaultMissingToken = tokenSet.getMissingInformationToken();
+		if (defaultMissingToken != null) {
+			writeSingleTokenDefinitionEvent(receiver, idManager, defaultMissingToken);  // Make sure the default missing data token in the first in the sequence.
+		}
+		
+		for (T token : tokenSet) {
+			if (!token.equals(defaultGapToken) && !token.equals(defaultMissingToken)) {
+				writeSingleTokenDefinitionEvent(receiver, idManager, token);
+			}
+		}
+		
+		//TODO JPhyloIO sollte auch mehrere Gap- und Missing-Tokens im Modell akzeptieren (falls später Formate auftauchen, die
+		//     das auch unterstützen). Bei Formaten, die nur eins zulassen, soll immer das erste (aus writeData()) verwendet werden
+    //     und die anderen als zusätzliche Zustände geschrieben werden.
+		//     In NeXML gäbe es weiterhin die Möglichkeit bei den alternativen Tokens entsprechende Metadaten zu schreiben. Dies 
+		//     könnte in JPhyloIO direkt geschehen oder aus LibrAlign oder der Anwendung. (Im Fall von AC würde man z.B. zu den 
+		//     supergaps weitere Informationen als nur "alternative Lücke" speichern wollen. (Außer bei einem evtl. Export wird
+		//     in AC allerdings ohnehin das Superalignment nicht direkt gespeichert, weshalb dort diese Tokens nicht vorkommen.)
+	}
+}
