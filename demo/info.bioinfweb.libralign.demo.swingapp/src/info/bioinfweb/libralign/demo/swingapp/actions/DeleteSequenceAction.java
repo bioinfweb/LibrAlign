a9bdd13a038c03eb9a47e109d6acb371c523486d
JonasBohn <bohn@bioinfweb.info>
2017-10-18 11:50:39 +0000

    Deleted confirm dialog for removing sequences. Minor changes.

--- demo/info.bioinfweb.libralign.demo.swingapp/src/info/bioinfweb/libralign/demo/swingapp/actions/DeleteSequenceAction.java
+++ demo/info.bioinfweb.libralign.demo.swingapp/src/info/bioinfweb/libralign/demo/swingapp/actions/DeleteSequenceAction.java
@@ -6,12 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.Action;
-import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 import info.bioinfweb.libralign.alignmentarea.selection.SelectionModel;
 import info.bioinfweb.libralign.demo.swingapp.SwingAlignmentEditor;
-
 
 
 
@@ -24,30 +22,14 @@ public class DeleteSequenceAction extends AbstractAlignmentEditorAction implemen
 		putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke('D', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
 	}
 
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switch(JOptionPane.showConfirmDialog(getEditor().getFrame(),
-				"Do you want to delete the choosen Sequences?", "Delete Sequence", JOptionPane.YES_NO_OPTION)) {
-		
-		case JOptionPane.YES_OPTION:
-			SelectionModel selection = getEditor().getAlignmentArea().getSelection();
-			
-			for (int row = selection.getFirstRow(); row <= selection.getLastRow(); row++) {
-				String id = getEditor().getAlignmentArea().getSequenceOrder().idByIndex(selection.getFirstRow());
-				getEditor().getAlignmentArea().getAlignmentModel().removeSequence(id);
-			}
-		case JOptionPane.NO_OPTION:
-			break;
-		}
+		SelectionModel selection = getEditor().getAlignmentArea().getSelection();
 
-		
-//		String sequenceID = getEditor().getAlignmentArea().getAlignmentModel().sequenceIDsByName(JOptionPane.showInputDialog("Name of sequence to be deleted")).iterator().next();
-//		if (getEditor().getAlignmentArea().getAlignmentModel().containsSequence(sequenceID)) {
-//			getEditor().getAlignmentArea().getAlignmentModel().removeSequence(sequenceID);
-//		}
-//		else {
-//			JOptionPane.showMessageDialog(getEditor().getFrame(), "Sequence not found");
-//		}		
+		for (int row = selection.getFirstRow(); row <= selection.getLastRow(); row++) {
+			String id = getEditor().getAlignmentArea().getSequenceOrder().idByIndex(selection.getFirstRow());
+			getEditor().getAlignmentArea().getAlignmentModel().removeSequence(id);
+		}
 	}	
 }
