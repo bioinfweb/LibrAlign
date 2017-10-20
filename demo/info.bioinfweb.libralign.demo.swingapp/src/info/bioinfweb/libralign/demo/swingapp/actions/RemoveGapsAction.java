b7c828a03623f0facbf0ead374eab737ef554c28
BenStoever <stoever@bioinfweb.info>
2017-10-20 10:41:19 +0000

    Copyright information added to Swing application demo.

--- demo/info.bioinfweb.libralign.demo.swingapp/src/info/bioinfweb/libralign/demo/swingapp/actions/RemoveGapsAction.java
+++ demo/info.bioinfweb.libralign.demo.swingapp/src/info/bioinfweb/libralign/demo/swingapp/actions/RemoveGapsAction.java
@@ -1,3 +1,21 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014-2017  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.bioinfweb.libralign.demo.swingapp.actions;
 
 
@@ -10,7 +28,6 @@ import javax.swing.KeyStroke;
 
 import info.bioinfweb.libralign.alignmentarea.selection.SelectionModel;
 import info.bioinfweb.libralign.demo.swingapp.SwingAlignmentEditor;
-
 
 
 
