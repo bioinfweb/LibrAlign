885ffd99658f2955ddbe1078abd4f15b8d1c7169
BenStoever <stoever@bioinfweb.info>
2017-10-20 10:40:55 +0000

    Copyright information added to documentation examples project. BasicAlignmentAreaDemo does not manually specify a token painter anymore. Initial versions of paper demo applications added.

---
+++ demo/info.bioinfweb.libralign.demo.docexamples/src/info/bionfweb/libralign/demo/paper/SWTApplication.java
@@ -0,0 +1,90 @@
+/*
+ * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
+ * Copyright (C) 2014-2017  Ben Stöver
+ * <http://bioinfweb.info/LibrAlign>
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */package info.bionfweb.libralign.demo.paper;
+
+
+import info.bioinfweb.libralign.alignmentarea.AlignmentArea;
+import info.bioinfweb.libralign.model.AlignmentModel;
+import info.bioinfweb.libralign.model.io.IOTools;
+import info.bioinfweb.tic.SWTComponentFactory;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+
+
+public class SWTApplication {
+	protected Shell shell;
+
+	
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			SWTApplication window = new SWTApplication();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Open the window.
+	 * @throws Exception 
+	 */
+	public void open() throws Exception {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+	
+	/**
+	 * Create contents of the window.
+	 * @throws Exception 
+	 */
+	protected void createContents() throws Exception {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		// Read an alignment file:
+		List<AlignmentModel<?>> models = IOTools.readAlignments(new File("data/Test.nexml"));
+		
+		// Create an alignment area displaying the first alignment from the file:
+		AlignmentArea area = new AlignmentArea();
+		area.setAlignmentModel(models.get(0), false);
+		
+		// Add the SWT version of alignment area to the GUI:
+		SWTComponentFactory.getInstance().getSWTComponent(area, shell, SWT.NONE);
+	}
+}
--- demo/info.bioinfweb.libralign.demo.docexamples/src/info/bionfweb/libralign/demo/paper/SwingApplication.java
+++ demo/info.bioinfweb.libralign.demo.docexamples/src/info/bionfweb/libralign/demo/paper/SwingApplication.java
@@ -1,40 +1,40 @@
 /*
  * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
- * Copyright (C) 2014-2015  Ben Stöver
+ * Copyright (C) 2014-2017  Ben Stöver
  * <http://bioinfweb.info/LibrAlign>
  * 
  * This file is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-package info.bionfweb.libralign.demo.docexamples;
+ */package info.bionfweb.libralign.demo.paper;
 
 
 import info.bioinfweb.libralign.alignmentarea.AlignmentArea;
-import info.bioinfweb.libralign.alignmentarea.tokenpainter.NucleotideTokenPainter;
-import info.bioinfweb.libralign.model.implementations.PackedAlignmentModel;
-import info.bioinfweb.libralign.model.tokenset.CharacterTokenSet;
+import info.bioinfweb.libralign.model.AlignmentModel;
+import info.bioinfweb.libralign.model.io.IOTools;
 import info.bioinfweb.tic.SwingComponentFactory;
 
-import java.awt.EventQueue;
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.io.File;
+import java.util.List;
 
 import javax.swing.JFrame;
 
 
 
-public class BasicAlignmentAreaDemo {
+public class SwingApplication {
 	private JFrame frame;
-	
+
 	
 	/**
 	 * Launch the application.
@@ -43,7 +43,7 @@ public class BasicAlignmentAreaDemo {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					BasicAlignmentAreaDemo window = new BasicAlignmentAreaDemo();
+					SwingApplication window = new SwingApplication();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -55,27 +55,32 @@ public class BasicAlignmentAreaDemo {
 	
 	/**
 	 * Create the application.
+	 * @throws Exception if an I/O error occurs
 	 */
-	public BasicAlignmentAreaDemo() {
+	public SwingApplication() throws Exception {
 		initialize();
 	}
 	
 	
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws Exception if an I/O error occurs
 	 */
-	private void initialize() {
+	private void initialize() throws Exception {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Swing application");
+		frame.setLayout(new BorderLayout());
 		
-		frame.getContentPane().add(SwingComponentFactory.getInstance().getSwingComponent(createAlignmentArea()), BorderLayout.CENTER);
-	}
-	
-	
-	private AlignmentArea createAlignmentArea() {
+		// Read an alignment file:
+		List<AlignmentModel<?>> models = IOTools.readAlignments(new File("data/Test.nexml"));
+		
+		// Create an alignment area displaying the first alignment from the file:
 		AlignmentArea area = new AlignmentArea();
-		area.setAlignmentModel(new PackedAlignmentModel<Character>(CharacterTokenSet.newNucleotideInstance()), false);  // Define a model
-		return area;
+		area.setAlignmentModel(models.get(0), false);
+		
+		// Add the Swing version of alignment area to the GUI:
+		frame.getContentPane().add(SwingComponentFactory.getInstance().getSwingComponent(area), BorderLayout.CENTER);
 	}
 }
