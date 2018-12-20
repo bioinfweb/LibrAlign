892e04ccb9a777580617e0bac0922fb573dbe008
LaraKoesters <LaraKoesters@bioinfweb.info>
2018-12-20 11:12:48 +0000

    When obtaining the currentSequenceID, not only the label but also the sequence ID provided by the file should be handed over to addSequence.

--- main/info.bioinfweb.libralign.io/src/info/bioinfweb/libralign/model/io/AlignmentModelEventReader.java
+++ main/info.bioinfweb.libralign.io/src/info/bioinfweb/libralign/model/io/AlignmentModelEventReader.java
@@ -330,7 +330,7 @@ public class AlignmentModelEventReader implements JPhyloIOEventListener {
 				if (event.getType().getTopologyType().equals(EventTopologyType.START)) {
 					LinkedLabeledIDEvent sequenceEvent = event.asLinkedLabeledIDEvent();
 					ensureCurrentModelInstance();
-					currentSequenceID = currentModel.addSequence(sequenceEvent.getLabel());  //TODO Handle case that no label is present or labels are not unique.
+					currentSequenceID = currentModel.addSequence(sequenceEvent.getLabel()/*, sequenceEvent.getID()*/);  //TODO Handle case that no label is present or labels are not unique.
 					//TODO Can't sequences be continued by an additional start event? It that case addSequence() should not be called again. => Create test case e.g. with MEGA or interleaved Nexus.
 				}
 				else {  // END
